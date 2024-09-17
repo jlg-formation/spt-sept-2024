e059ea15eae72d7813fd4ce4560ff02123084b32
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-17 15:16:52 +0200

    dev: added circles point

--- front/main.js
+++ front/main.js
@@ -1,1 +1,21 @@
 console.log("coucou");
+
+const samples = 10;
+
+const svgns = "http://www.w3.org/2000/svg";
+
+const r = 1;
+
+const container = document.querySelector("g.samples");
+if (container === null) {
+  throw new Error("zut");
+}
+
+for (let i = 0; i < samples; i++) {
+  // add a point
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", "50");
+  circle.setAttributeNS(null, "cy", "50");
+  circle.setAttributeNS(null, "r", r + "");
+  container.appendChild(circle);
+}
