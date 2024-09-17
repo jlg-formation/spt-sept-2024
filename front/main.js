8e3778d14714b941b144ec812831c012f27d66a7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-17 16:20:23 +0200

    dev: add lines

--- front/main.js
+++ front/main.js
@@ -1,10 +1,14 @@
 console.log("coucou");
 
 const samples = 10;
+const multiplicationFactor = 2;
 
 const svgns = "http://www.w3.org/2000/svg";
 
 const r = 1;
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
 
 const container = document.querySelector("g.samples");
 if (container === null) {
@@ -13,9 +17,38 @@ if (container === null) {
 
 for (let i = 0; i < samples; i++) {
   // add a point
+  const angle = i * ((2 * Math.PI) / samples);
+
+  const x = cx0 + r0 * Math.cos(angle);
+  const y = cy0 + r0 * Math.sin(angle);
+
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", "50");
-  circle.setAttributeNS(null, "cy", "50");
+  circle.setAttributeNS(null, "cx", x + "");
+  circle.setAttributeNS(null, "cy", y + "");
   circle.setAttributeNS(null, "r", r + "");
   container.appendChild(circle);
+}
+
+const lineContainer = document.querySelector("g.lines");
+if (lineContainer === null) {
+  throw new Error("zut");
+}
+
+for (let i = 0; i < samples; i++) {
+  // add a line
+  const angle1 = i * ((2 * Math.PI) / samples);
+  const angle2 = angle1 * multiplicationFactor;
+
+  const x1 = cx0 + r0 * Math.cos(angle1);
+  const y1 = cy0 + r0 * Math.sin(angle1);
+
+  const x2 = cx0 + r0 * Math.cos(angle2);
+  const y2 = cy0 + r0 * Math.sin(angle2);
+
+  const line = document.createElementNS(svgns, "line");
+  line.setAttributeNS(null, "x1", x1 + "");
+  line.setAttributeNS(null, "y1", y1 + "");
+  line.setAttributeNS(null, "x2", x2 + "");
+  line.setAttributeNS(null, "y2", y2 + "");
+  lineContainer.appendChild(line);
 }
